73640da959b01936b6649a8fe277574ba94299dd
Marcelo Alves <alvesma@gmail.com>
2021-03-18 10:01:20 -0300

    Projeto Criado

---
+++ src/main/java/com/biblio/BibliomaApplication.java
@@ -0,0 +1,13 @@
+package com.biblio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliomaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliomaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biblio/BibliomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
