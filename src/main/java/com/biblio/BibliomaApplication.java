faf750172ab768a7f3e86e6ba3e02b3e5d25c2e5
Marcelo Alves <alvesma@gmail.com>
2021-03-19 10:10:46 -0300

    Operacao Instanciacao

--- src/main/java/com/biblio/BibliomaApplication.java
+++ src/main/java/com/biblio/BibliomaApplication.java
@@ -1,13 +1,33 @@
 package com.biblio;
 
+import java.util.Arrays;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.biblio.biblio.domain.Categoria;
+import com.biblio.biblio.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class BibliomaApplication {
+public class BibliomaApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BibliomaApplication.class, args);
 	}
 
-}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
+} 
