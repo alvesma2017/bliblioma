0b9d6863138a457b68eca2866e5c3203b2da6e06
Marcelo Alves <alvesma@gmail.com>
2021-03-19 09:49:35 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/biblio/biblio/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.biblio.biblio.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+import com.biblio.biblio.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository <Categoria, Integer> {
+
+}
---
+++ src/main/java/com/biblio/biblio/services/CategoriaServices.java
@@ -0,0 +1,21 @@
+package com.biblio.biblio.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.biblio.biblio.domain.Categoria;
+import com.biblio.biblio.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaServices {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
