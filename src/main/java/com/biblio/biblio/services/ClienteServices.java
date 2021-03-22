927bd82f976cc9e39d9c8c4610ed2ac8e332543e
Marcelo Alves <alvesma@gmail.com>
2021-03-22 17:12:39 -0300

    PUT, DELETE E GET Clientes

--- src/main/java/com/biblio/biblio/services/ClienteServices.java
+++ src/main/java/com/biblio/biblio/services/ClienteServices.java
@@ -1,13 +1,22 @@
 package com.biblio.biblio.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.biblio.biblio.domain.Cliente;
+
 import com.biblio.biblio.repositories.ClienteRepository;
+import com.biblio.biblio.services.exceptions.DataIntegrityException;
 import com.biblio.biblio.services.exceptions.ObjectNotFoundException;
+import com.biblio.dto.ClienteDTO;
+
 
 
 @Service
@@ -26,7 +35,35 @@ public class ClienteServices {
 		
 		
 	}
-		
-		
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
-
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um cliente porque possui endereços associados");
+		}
+	}
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	
+	}
+}
