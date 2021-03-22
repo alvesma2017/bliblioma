3839ae118fd44b5bb3f4fff999830b1061635095
Marcelo Alves <alvesma@gmail.com>
2021-03-22 10:31:07 -0300

    Atualizando Categoria + alterações pedidos/clientes

--- src/main/java/com/biblio/resources/ClienteResource.java
+++ src/main/java/com/biblio/resources/ClienteResource.java
@@ -4,6 +4,7 @@ package com.biblio.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,10 +21,12 @@ public class ClienteResource {
 	private ClienteServices service;
 	
 		@RequestMapping (value= "/{id}", method=RequestMethod.GET)
-		public ResponseEntity<?> find(@PathVariable Integer id) {
+		public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 			Cliente obj = service.find(id);
 			return ResponseEntity.ok().body(obj);
 		
 		 } 
+		
 
+		
 }
