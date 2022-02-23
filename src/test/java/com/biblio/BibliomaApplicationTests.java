37f55734323e1431bf599b57735e93aeb6b8f804
alvesma2017 <alvesma@gmail.com>
2022-02-23 18:38:47 -0300

    TESTE 230222

--- src/test/java/com/biblio/BibliomaApplicationTests.java
+++ src/test/java/com/biblio/BibliomaApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BibliomaApplicationTests {
+class BibliomaApplicationTests { tttt
 
 	@Test
 	void contextLoads() {
